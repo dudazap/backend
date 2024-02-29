6809c7a1ccf6d1fb15c674a99dc151cf9b698d0b
dudazap <125655570+dudazap@users.noreply.github.com>
2024-02-29 11:27:58 -0300

    Add files via upload

---
+++ eduarda/script.js
@@ -0,0 +1,13 @@
+let nome;
+nome = prompt("Digite o seu nome:");
+console.log(nome);
+window.alert(`Olá ${nome}!`);
+
+let idade;
+nome = prompt("Digite sua idade:");
+console.log(idade);
+var datadenascimento = (2024 - idade);
+window.alert(`Você naceu em ${datadenascimento}!`);
+
+let numeros [20];
+for (let i = 0; i<20)
