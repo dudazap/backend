a27257ea312935a210ba9f89fe113ed60a90b940
dudazap <125655570+dudazap@users.noreply.github.com>
2024-03-07 08:15:15 -0300

    Add files via upload

--- eduarda/script.js
+++ eduarda/script.js
@@ -4,10 +4,24 @@ console.log(nome);
 window.alert(`Olá ${nome}!`);
 
 let idade;
-nome = prompt("Digite sua idade:");
+idade = prompt("Digite sua idade:");
 console.log(idade);
 var datadenascimento = (2024 - idade);
 window.alert(`Você naceu em ${datadenascimento}!`);
 
-let numeros [20];
-for (let i = 0; i<20)
+let numeros = [];
+for (let i = 0; i<20 ; i ++) {numeros [i] = Number.parseInt(Math.random () * 100 +1) ; }
+console.log(numeros)
+
+var tentativas = 0
+do {
+    let chute ;
+    chute = prompt('Chute');
+
+let acertou = false
+
+    for (let i = 0; i<20 ; i ++) {
+        if (chute == numeros [i] ) acertou = true}
+        if (acertou) {window.alert ('Parabens'); break}
+} while ( tentativas < 3);
+
